f2e25f7530f38fb08c87a7d7796515c7a725890a
Markus Pawel <Markus.Pawel@polyshell.net>
2021-09-23 17:07:45 +0200

    Create __main__.py for packaging

---
+++ __main__.py
@@ -0,0 +1,23 @@
+import configparser
+import os
+from src.poller import PagePoller
+from discord import Webhook, RequestsWebhookAdapter
+
+import time
+
+PRIVATE_CONFIG_FILE = "private.config"
+
+print(os.path.join(os.path.dirname(__file__), PRIVATE_CONFIG_FILE))
+private_config = configparser.ConfigParser()
+private_config.read(os.path.join(os.path.dirname(__file__), PRIVATE_CONFIG_FILE))
+
+webhook = Webhook.from_url(private_config.get('Webhooks', 'bot-status'), adapter=RequestsWebhookAdapter())
+
+while True:
+    webhook.send("Running...")
+
+    pagepoller = PagePoller(1, True, True, True)
+    pagepoller.private_config = private_config
+    pagepoller.check_website()
+
+    time.sleep(30)
