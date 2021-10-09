ee99d6bf01f6bcb0ce4c92ca36808da4a8312495
Parama91 <r.pawel91@hotmail.com>
2021-10-10 00:05:04 +0200

    Add electricity cost and efficiency calculation

--- __main__.py
+++ __main__.py
@@ -19,18 +19,30 @@ print("reading config ...")
 with open("resources/config.json") as config_json:
     config = json.load(config_json)
 
-# ETH data
+price_kWh = 0.11
+
+# actual mega hash rate (all 4 cards together)
+mega_hash_rate = 141.88
+watt = 325
+
+# efficiency on NiceHash
+efficiency = mega_hash_rate / watt
+print(efficiency)
+
+# ETH data from https://api.minerstat.com/
 eth_data = requests.get("https://api.minerstat.com/v2/coins?list=eth").json()[0]
 difficulty = eth_data["difficulty"]
 reward = eth_data["reward_block"]
 price = eth_data["price"]
 
-# actual mega hash rate (all 4 cards together)
-mega_hash_rate = 141.88
-
 # ((hash_rate [h/s] * reward) / difficulty) * (1 - pool_fee) * 3600 * 24
 dollar_per_day = ((mega_hash_rate * 1000000 * reward) / difficulty) * (1 - 0.02) * 3600 * 24 * price
 print(dollar_per_day)
+
+# actual watt consumption (all 4 cards together)
+kWh = watt * 24 / 1000
+price = price_kWh * kWh
+print(price)
 
 pagepoller = PagePoller(False, True, True, private_config, config)
 
