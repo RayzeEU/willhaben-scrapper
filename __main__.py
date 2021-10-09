cf973fc69d6c0919bc5174aa28c0101c70bc34e0
Parama91 <r.pawel91@hotmail.com>
2021-10-09 23:39:03 +0200

    Add first calculation for profit on ETH based on hash rate, difficulty, reward per block and ETH price (only available in dollar)

--- __main__.py
+++ __main__.py
@@ -2,6 +2,7 @@ import configparser
 import os
 import json
 import time
+import requests
 
 from src.poller import PagePoller
 from discord import Webhook, RequestsWebhookAdapter
@@ -17,6 +18,19 @@ webhook = Webhook.from_url(private_config.get('Webhooks', 'bot-status'), adapter
 print("reading config ...")
 with open("resources/config.json") as config_json:
     config = json.load(config_json)
+
+# ETH data
+eth_data = requests.get("https://api.minerstat.com/v2/coins?list=eth").json()[0]
+difficulty = eth_data["difficulty"]
+reward = eth_data["reward_block"]
+price = eth_data["price"]
+
+# actual hashrate (all 4 cards together)
+hash_rate = 141880000
+
+# ((hash_rate [h/s] * reward) / difficulty) * (1 - pool_fee) * 3600 * 24
+dollar_per_day = ((hash_rate * reward) / difficulty) * (1 - 0.02) * 3600 * 24 * price
+print(dollar_per_day)
 
 pagepoller = PagePoller(False, True, True, private_config, config)
 
