60ba153a0417176adfa950e9d4d51c735256c598
Parama91 <r.pawel91@hotmail.com>
2021-10-23 00:21:10 +0200

    Kalkulation angepasst -> muss in eine eigene Klasse verschoben werden

--- __main__.py
+++ __main__.py
@@ -19,11 +19,11 @@ print("reading config ...")
 with open("resources/config.json") as config_json:
     config = json.load(config_json)
 
-price_kWh = 0.11
+price_kWh = 0.19
 
 # actual mega hash rate (all 4 cards together)
-mega_hash_rate = 141.88
-watt = 325
+mega_hash_rate = 196.5
+watt = 452
 
 # efficiency on NiceHash
 efficiency = mega_hash_rate / watt
@@ -33,18 +33,18 @@ print("efficiency: {0} MH/J".format(round(efficiency, 4)))
 eth_data = requests.get("https://api.minerstat.com/v2/coins?list=eth").json()[0]
 difficulty = eth_data["difficulty"]
 reward = eth_data["reward_block"]
-price = eth_data["price"]
+price = eth_data["price"] / 1.16
 
 # ((hash_rate [h/s] * reward) / difficulty) * (1 - pool_fee) * 3600 * 24
 dollar_per_day = ((mega_hash_rate * 1000000 * reward) / difficulty) * (1 - 0.02) * 3600 * 24 * price
-print("daily: {0} $".format(round(dollar_per_day, 2)))
+print("daily: {0} €".format(round(dollar_per_day, 2)))
 
 # actual watt consumption (all 4 cards together)
 kWh = watt * 24 / 1000
 price = price_kWh * kWh
-print("electricity daily: {0} $".format(round(price, 2)))
+print("electricity daily: {0} €".format(round(price, 2)))
 
-print("daily after electricity: {0} $".format(round(dollar_per_day - price, 2)))
+print("daily after electricity: {0} €".format(round(dollar_per_day - price, 2)))
 
 pagepoller = PagePoller(False, True, True, private_config, config)
 
