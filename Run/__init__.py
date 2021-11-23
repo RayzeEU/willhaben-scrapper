b39a83c6f3ad8fb67166e7f5c85e0f563bbdf19d
Markus Pawel <Markus.Pawel@polyshell.net>
2021-11-23 11:37:47 +0100

    Update requirements.txt and update logging

--- Run/__init__.py
+++ Run/__init__.py
@@ -1,10 +1,8 @@
 import datetime
 import logging
 import os
-import configparser
 import json
 import requests
-import time
 
 import azure.functions as func
 
@@ -25,7 +23,7 @@ def main(mytimer: func.TimerRequest) -> None:
     webhook = Webhook.from_url(os.environ["Discord_Bot_Status"], adapter=RequestsWebhookAdapter())
     webhook.send("Test")
 
-    print("reading config ...")
+    logging.info("reading config ...")
     with open("resources/config.json") as config_json:
         config = json.load(config_json)
 
@@ -37,7 +35,7 @@ def main(mytimer: func.TimerRequest) -> None:
 
     # efficiency on NiceHash
     efficiency = mega_hash_rate / watt
-    print("efficiency: {0} MH/J".format(round(efficiency, 4)))
+    logging.info("efficiency: {0} MH/J".format(round(efficiency, 4)))
 
     # ETH data from https://api.minerstat.com/
     eth_data = requests.get("https://api.minerstat.com/v2/coins?list=eth").json()[0]
@@ -47,13 +45,13 @@ def main(mytimer: func.TimerRequest) -> None:
 
     # ((hash_rate [h/s] * reward) / difficulty) * (1 - pool_fee) * 3600 * 24
     dollar_per_day = ((mega_hash_rate * 1000000 * reward) / difficulty) * (1 - 0.02) * 3600 * 24 * price
-    print("daily: {0} €".format(round(dollar_per_day, 2)))
+    logging.info("daily: {0} €".format(round(dollar_per_day, 2)))
 
     # actual watt consumption (all 4 cards together)
     kWh = watt * 24 / 1000
     price = price_kWh * kWh
-    print("electricity daily: {0} €".format(round(price, 2)))
+    logging.info("electricity daily: {0} €".format(round(price, 2)))
 
-    print("daily after electricity: {0} €".format(round(dollar_per_day - price, 2)))
+    logging.info("daily after electricity: {0} €".format(round(dollar_per_day - price, 2)))
 
     PagePoller(True, False, os.environ["Discord_Latest_Cards"], config).check_website()
