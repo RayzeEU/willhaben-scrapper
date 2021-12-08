16ef992f895440f088d96d6645246c5e4b146eb3
Parama91 <r.pawel91@hotmail.com>
2021-12-08 16:57:28 +0100

    Add __init__.py for lokal testing, do analysis also on dev Branch, add private_config.json to .gitignore

---
+++ src/__init__.py
@@ -0,0 +1,19 @@
+import json
+import logging
+import os
+
+from src.poller import PagePoller
+
+logging.basicConfig(level=logging.INFO)
+
+logging.info("reading private config ...")
+with open("../resources/private_config.json") as private_config_json:
+    private_config = json.load(private_config_json)
+    os.environ["Discord_Latest_Cards"] = private_config["Discord_Latest_Cards"]
+    os.environ["Discord_Bot_Status"] = private_config["Discord_Bot_Status"]
+
+logging.info("reading config ...")
+with open("../resources/config.json") as config_json:
+    config = json.load(config_json)
+
+PagePoller(True, False, config).check_website()
